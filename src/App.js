660606c861772f21b38780eae9511e100c6de086
BKI <b.kisfali@gmail.com>
2017-11-16 19:33:17 +0100

    redo one more time based on instructions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import BusinessList from './components/Business/Business.js';
-import SearchBar from './components/SearchBar/SearchBar.js';
+import BusinessList from './components/Business/Business';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
   <h1>ravenous</h1>
-  {<SearchBar />}
-  {<BusinessList />}
+  <SearchBar />
+  <BusinessList />
 </div>
     );
   }
