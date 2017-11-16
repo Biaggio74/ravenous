bd89adc00ac2f9cdd1eda3e85f4a8d4af596ab7b
BKI <b.kisfali@gmail.com>
2017-11-16 21:02:56 +0100

    new push for debugging

--- src/App.js
+++ src/App.js
@@ -16,3 +16,21 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+export class NavBar extends React.Component {
+  render() {
+    const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+    const navLinks = pages.map(page => {
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      )
+    });
+
+    return <nav>{navLinks}</nav>;
+  }
+}
+
+*/
